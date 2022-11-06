2ffb753baca3664669a650f284534b028011ef88
aheeshnagraj <aheesh@gmail.com>
2022-11-06 01:55:25 -0700

    basic NFT minting and assignment to receiver address

--- contracts/op3nMktNFT.sol
+++ contracts/op3nMktNFT.sol
@@ -1,9 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract op3nMktNFT {
+contract Op3nMktNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
+    constructor() ERC721("Op3nMkt", "OP3N") {}
+
+    function mintOp3n(address toAddress, string memory tokenURI) public returns (uint256) {
+        uint256 newItemId = _tokenIds.current();
+        _mint(toAddress, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        _tokenIds.increment();
+        return newItemId;
+    }
 }
